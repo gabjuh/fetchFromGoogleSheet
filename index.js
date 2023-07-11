d45d00b2b67c8b03dd24a087132ea5b8bff5c3d5
gabjuh <jjjjgabesz@gmail.com>
2023-07-11 07:18:55 +0200

    create json from all sheet data

--- index.js
+++ index.js
@@ -4,33 +4,43 @@ const fs = require('fs');
 
 const app = express();
 const port = 3003; // Change this if needed
-const openSheetApiUrl = 'https://opensheet.elk.sh/';
+const openSheetApiUrl = 'https://opensheet.vercel.app/';
 const tableId = '1ghHCdIe2HEsWZ67Mit9WqcsAZUr99s68a495Kn-D-VI';
-const sheetName = 'home';
+
+let obj = {};
 
 app.get('/fetch-data', async (req, res) => {
   try {
-    // const response = await axios.get('https://opensheet.vercel.app/YOUR_SPREADSHEET_ID'); // Replace with your Google Sheet ID
-    // const response = await axios.get(`https://opensheet.elk.sh/1ghHCdIe2HEsWZ67Mit9WqcsAZUr99s68a495Kn-D-VI/home`); // Replace with your Google Sheet ID
-    const response = await axios.get(`https://opensheet.vercel.app/1ghHCdIe2HEsWZ67Mit9WqcsAZUr99s68a495Kn-D-VI/home`); // Replace with your Google Sheet ID
-    // 1ghHCdIe2HEsWZ67Mit9WqcsAZUr99s68a495Kn-D-VI
-    // https://opensheet.elk.sh
-    // https://opensheet.vercel.app
-    // https://docs.google.com/spreadsheets/d/1ghHCdIe2HEsWZ67Mit9WqcsAZUr99s68a495Kn-D-VI/edit?usp=sharing
+    let isResponse = true;
 
-    // const response = fetch(`${openSheetApiUrl}${tableId}/${sheetName}`)
-      // .then((res) => res.json());
-    
-    // Extract the data from the response
-    const data = response.data;
-    
+    for (let i = 1; isResponse; i++) {
+      // Fetch the data from the Google Sheet
+      const response = await axios.get(`${openSheetApiUrl}${tableId}/${i}`); 
+
+      if (response.status !== 400) {
+        // Extract the data from the response
+        const data = response.data;
+        obj[data[0].sheetId] = data;
+      } else {
+        isResponse = false;
+        console.log('No response');
+      }
+    }
+
     // Save the data as a JSON file
-    fs.writeFileSync('data.json', JSON.stringify(data, null, 2));
-    
+    fs.writeFileSync('data.json', JSON.stringify(obj, null, 2));
+
     res.send('Data fetched and saved as data.json');
   } catch (error) {
-    console.error('Error fetching data:', error);
-    res.status(500).send('Error fetching data');
+    if (error.response && error.response.status === 400) {
+      // Save the data as a JSON file even if there was a 400 status
+      fs.writeFileSync('data.json', JSON.stringify(obj, null, 2));
+      console.log('No more data to fetch');
+      res.send('Data fetched and saved as data.json');
+    } else {
+      console.error('Error fetching data:', error);
+      res.status(500).send('Error fetching data');
+    }
   }
 });
 
